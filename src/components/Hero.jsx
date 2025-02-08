222a3d9b08f73d088758ad40401534c94a30429e
Atharva Dhumal <atharvaadhumal@gmail.com>
2025-02-08 20:35:32 +0530

    added NEXUS logo in Hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import heroimg from '../assets/images/nexusheroimig.jpg'
+import logo from '../assets/images/nexusLogo.png';
 
 const Hero = () => {
   return (
@@ -14,9 +15,16 @@ const Hero = () => {
         </div>
         <div className="absolute inset-0 flex items-center justify-center">
           <div className="text-center">
-            <h1 className="text-4xl sm:text-6xl md:text-7xl lg:text-8xl font-extrabold text-white tracking-wide">
-              NEXUS
-            </h1> 
+            <div className="flex items-center justify-center gap-2">
+              <img 
+                src={logo} 
+                alt="Nexus Logo" 
+                className="h-[3rem] sm:h-[4.5rem] md:h-[5.5rem] lg:h-[6.5rem] w-auto"
+              />
+              <h1 className="text-4xl sm:text-6xl md:text-7xl lg:text-8xl font-extrabold text-white tracking-wide">
+                NEXUS
+              </h1> 
+            </div>
             <p className=" text-base sm:text-2xl md:text-3xl text-white">
               Your Partner in Precision and Innovation
             </p>
