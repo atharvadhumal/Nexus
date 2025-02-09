89e514b9df0067f1092e574266125c6c5fd3d29c
Atharva Dhumal <atharvaadhumal@gmail.com>
2025-02-09 15:33:43 +0530

    removed designedBy path

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
-import DesignedBy from '../components/DesignedBy'
+
 
 const MainLayout = () => {
   return (
@@ -10,7 +10,7 @@ const MainLayout = () => {
     <Navbar />
     <Outlet/>
     <Footer />
-    <DesignedBy />
+
     
     </>
   )
